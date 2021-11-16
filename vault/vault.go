77204f741ac7b217b4a2d9886237a99c2a9c6b07
Sergio Rua <sergio.rua@digitalis.io>
2021-11-16 09:58:44 +0000

    Kube login for Vault and token renewal

---
+++ vault/vault.go
@@ -0,0 +1,183 @@
+package vault
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/hashicorp/vault/api"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const (
+	kubernetesJwtTokenPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	kubernetesAuthUrl      = "auth/kubernetes/login"
+)
+
+var log logr.Logger
+
+func fileExists(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
+
+func vaultClient() (*api.Client, error) {
+	var client *api.Client
+	var err error
+	var vaultSkipVerify bool = false
+
+	vaultUrl := os.Getenv("VAULT_ADDR")
+	if os.Getenv("VAULT_SKIP_VERIFY") != "" && os.Getenv("VAULT_SKIP_VERIFY") == "true" {
+		vaultSkipVerify = true
+	}
+	if vaultUrl == "" {
+		return client, fmt.Errorf("VAULT_ADDR is not set")
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: vaultSkipVerify},
+	}
+
+	httpClient := &http.Client{Transport: tr}
+
+	// create a vault client
+	client, err = api.NewClient(&api.Config{Address: vaultUrl, HttpClient: httpClient})
+	if err != nil {
+		return client, err
+	}
+
+	return client, err
+}
+
+func renewToken() (float64, error) {
+	var tokenTTL float64
+	client, err := vaultClient()
+	if err != nil {
+		return -1, err
+	}
+
+	r, err := client.Logical().Read("auth/token/lookup-self")
+	if err != nil {
+		return -1, err
+	}
+
+	t, err := r.TokenTTL()
+	if err != nil {
+		return -1, err
+	}
+	tokenTTL = t.Seconds()
+
+	/* FIXME: is this a sensible minimum? */
+	if tokenTTL > 120 {
+		return tokenTTL, nil
+	}
+
+	if ok, err := r.TokenIsRenewable(); !ok {
+		return tokenTTL, err
+	}
+
+	r, err = client.Logical().Write("/auth/token/renew-self", map[string]interface{}{
+		"token": os.Getenv("VAULT_TOKEN"),
+	})
+	if err != nil {
+		return tokenTTL, err
+	}
+
+	tokenTTL = float64(r.Auth.LeaseDuration)
+	log.Info(fmt.Sprintf("Vault token renewed. New TTL is %v seconds", tokenTTL))
+
+	return tokenTTL, nil
+}
+
+func kubeLogin() error {
+	if ok, err := fileExists(kubernetesJwtTokenPath); !ok {
+		return err
+	}
+	if os.Getenv("VAULT_ROLE_ID") == "" {
+		return fmt.Errorf("VAULT_ROLE_ID not defined")
+	}
+	client, err := vaultClient()
+	if err != nil {
+		return err
+	}
+	fd, err := os.Open(kubernetesJwtTokenPath)
+	defer fd.Close()
+	if err != nil {
+		return err
+	}
+
+	jwt, err := ioutil.ReadAll(fd)
+	if err != nil {
+		return err
+	}
+
+	params := map[string]interface{}{
+		"jwt":  string(jwt),
+		"role": os.Getenv("VAULT_ROLE_ID"),
+	}
+
+	var loginUrl string
+	if os.Getenv("VAULT_KUBERNETES_PATH") == "" {
+		loginUrl = kubernetesAuthUrl
+	} else {
+		loginUrl = fmt.Sprintf("auth/%s/login", os.Getenv("VAULT_KUBERNETES_PATH"))
+	}
+
+	r, err := client.Logical().Write(loginUrl, params)
+	if err != nil {
+		return err
+	}
+
+	client.SetToken(r.Auth.ClientToken)
+
+	err = os.Setenv("VAULT_TOKEN", r.Auth.ClientToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Start() error {
+	log = ctrl.Log.WithName("vault")
+
+	if os.Getenv("VAULT_AUTH_METHOD") == "kubernetes" {
+		err := kubeLogin()
+		if err != nil {
+			return err
+		}
+	}
+	go func() {
+		for {
+			tokenTTL, err := renewToken()
+			if err != nil {
+				log.Error(err, "Error renewing vault token")
+				continue
+			}
+			log.Info(fmt.Sprintf("Vault token will expire in %v seconds", tokenTTL))
+			/* Wait for near the time when token expires */
+			if tokenTTL > 0 {
+				var sleepTime float64 = tokenTTL
+				if tokenTTL > 120 {
+					sleepTime = sleepTime - 120
+				}
+				time.Sleep(time.Second * time.Duration(sleepTime))
+			} else {
+				time.Sleep(time.Second * 60)
+			}
+		}
+	}()
+
+	return nil
+}
