211a5fe70e02fea15469e67e0afc5d9c6cd97bbc
Sergio Rua <sergio.rua@digitalis.io>
2023-06-13 09:07:31 +0100

    Tagging

---
+++ scripts/tag.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+versionNumber=${1}
+
+if [ -z $versionNumber ]; then
+    echo "Usage: $(basename $0) <a.b.c>"
+    exit 1
+fi
+
+if [[ $versionNumber =~ ^[0-9]+\.[0-9]+ ]]; then
+    tag="v${versionNumber}"
+    git rev-list $tag > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Tag ${tag} already exists"
+        exit 1
+    fi
+
+    if [ ! -f charts/vals-operator/Chart.yaml ]; then
+        echo "Please run from the git repo root directory"
+        exit 1
+    fi
+
+    sed -i '' "s/version:.*/version: ${versionNumber}/g" charts/vals-operator/Chart.yaml
+    sed -i '' "s/appVersion:.*/appVersion: ${tag}/g" charts/vals-operator/Chart.yaml
+
+    git commit -S -am "Tagging version ${versionNumber}" && git push && \
+    git tag $tag && git push --tags
+fi
